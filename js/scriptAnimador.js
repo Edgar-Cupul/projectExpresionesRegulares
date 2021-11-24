f10be0b33fde2ef9f449ff4a1225085e66ead34b
Edgar Cupul <a18016876@alumnos.uady.mx>
2021-11-24 08:32:49 -0600

    Se ha agregado animaciones y botones a los modal

---
+++ js/scriptAnimador.js
@@ -0,0 +1,128 @@
+
+class Timer {
+    constructor(fn, t) {
+        var timerObj = setInterval(fn, t);
+
+        this.stop = function () {
+            if (timerObj) {
+                clearInterval(timerObj);
+                timerObj = null;
+            }
+            return this;
+        };
+
+        // start timer using current settings (if it's not already running)
+        this.start = function () {
+            if (!timerObj) {
+                this.stop();
+                timerObj = setInterval(fn, t);
+            }
+            return this;
+        };
+
+        // start with new interval, stop current interval
+        this.reset = function (newT) {
+            t = newT;
+            return this.stop().start();
+        };
+
+        //alert("Ready function timer");
+    }
+}//Funciona como una clase.
+
+/**
+ * 
+ * @param {*} frames Número de frames de la animacion
+ * @param {*} direc Dirección de la animacion
+ * @param {*} animacion Nombre de la animación
+ * @param {*} closer Nombre de el icono que cierra la ventana modal.
+ */
+function Animador(frames, direc, animacion, closer) {
+    this.frames = frames;
+    this.direc = direc;
+    this.animacion = animacion;
+    this.closer = closer;
+
+    this.testFunct = function () {
+        console.log("You have now: " + frames + "," + direc + ", " + animacion);
+
+        //alert("Passed!");
+    }
+
+
+    this.animate = function () {
+        var diapositiva = new Image();
+
+        //Variables para contar
+        var contadorDeFrames = 1;
+
+        const inputVelocidad = document.querySelector("#" + animacion + "v");
+        const salidaVelocidad = document.querySelector("#out-" + animacion + "v"); //Se completa con out, debe ser similar a la entrada.
+        const botonVelocidad = document.querySelector("#boton-" + animacion);
+        const botonStoper = document.getElementById(''+closer);
+
+        botonStoper.onclick = () => {
+            contadorDeFrames = 1;
+            control.reset();
+            control.stop();
+        }
+
+        inputVelocidad.oninput = () => {
+            //reiniciamos el contador de frames al mover algo acá.
+            contadorDeFrames = 1;
+
+            salidaVelocidad.innerHTML = inputVelocidad.value;
+            velocidadAnimacion = parseInt(inputVelocidad.value);
+            control.reset(parseInt(inputVelocidad.value));
+            //alert("Dato recibido:"+parseInt(inputVelocidad.value));
+        }
+
+        botonVelocidad.onclick = () => {
+            if(botonVelocidad.innerHTML == "Pausa"){
+                control.stop();
+                botonVelocidad.innerHTML = "Reanudar";
+            }else{
+                control.start();
+                botonVelocidad.innerHTML = "Pausa";
+            }
+        }
+
+        var maxValue = parseInt(inputVelocidad.max);
+        var minValue = parseInt(inputVelocidad.min);
+
+        var defaultValue = (maxValue < minValue) ? minValue
+            : minValue + (maxValue - minValue) / 2;
+
+        inputVelocidad.value = defaultValue;
+
+        var velocidadAnimacion = parseInt(defaultValue);
+
+
+        var control = new Timer(function animador() {
+            console.log("Mostrando este mensaje cada:" + velocidadAnimacion);
+            var limite = 0;
+            var intervalo
+
+            var canvas = document.getElementById(animacion+"c");
+            var ctx = canvas.getContext('2d');
+
+            if (contadorDeFrames == frames + 2) {
+                alert("Ha habido un error de conteo. Recarga la página por favor.");
+            }
+
+            diapositiva.src = direc + contadorDeFrames + ".png";
+            ctx.drawImage(diapositiva, 60, 0, 700, 393);
+
+            contadorDeFrames += 1;
+
+            if (contadorDeFrames > frames+1) {
+                control.stop();
+                //alert(velocidadAnimacion);
+                //alert("Proceso concluido.")
+            }
+
+        }, velocidadAnimacion);
+
+    }
+
+}
--- js/scriptModals.js
+++ js/scriptModals.js
@@ -2,13 +2,17 @@ let modalUnion1 = document.getElementById('modalUnion1');
 let flex1 = document.getElementById('flex1');
 let abrir1 = document.getElementById('abrir1');
 let cerrar1 = document.getElementById('close1');
+var animacionUnion1 = new Animador(23, 'img/animation/union/dosCaminos/', "dosCaminos", 'close1');
 
 abrir1.addEventListener('click', function(){
     modalUnion1.style.display = 'block';
+    animacionUnion1.animate();
+
 });
 
 cerrar1.addEventListener('click', function(){
     modalUnion1.style.display = 'none';
+
 });
 
 window.addEventListener('click', function(e){
@@ -24,9 +28,12 @@ let modalUnion2 = document.getElementById('modalUnion2');
 let flex2 = document.getElementById('flex2');
 let abrir2 = document.getElementById('abrir2');
 let cerrar2 = document.getElementById('close2');
+var animacionUnion2 = new Animador(15, 'img/animation/union/dosExpresionesConUnion/', "dosExpresionesConUnion",'close2');
 
 abrir2.addEventListener('click', function(){
     modalUnion2.style.display = 'block';
+    animacionUnion2.animate();
+
 });
 
 cerrar2.addEventListener('click', function(){
@@ -46,9 +53,12 @@ let modalUnion3 = document.getElementById('modalUnion3');
 let flex3 = document.getElementById('flex3');
 let abrir3 = document.getElementById('abrir3');
 let cerrar3 = document.getElementById('close3');
+var animacionUnion3 = new Animador(13, 'img/animation/union/dosExpresionesSinUnion/', "dosExpresionesSinUnion",'close3');
 
 abrir3.addEventListener('click', function(){
     modalUnion3.style.display = 'block';
+    animacionUnion3.animate();
+
 });
 
 cerrar3.addEventListener('click', function(){
@@ -68,9 +78,11 @@ let modalConcatenacion = document.getElementById('modalConcatenacion');
 let flex4 = document.getElementById('flex4');
 let abrir4 = document.getElementById('abrir4');
 let cerrar4 = document.getElementById('close4');
+var animacionConcatenacion = new Animador(19, 'img/animation/expresionesConcatenadas/', "expresionesConcatenadas",'close4');
 
 abrir4.addEventListener('click', function(){
     modalConcatenacion.style.display = 'block';
+    animacionConcatenacion.animate();
 });
 
 cerrar4.addEventListener('click', function(){
@@ -90,9 +102,12 @@ let modalCierreEstrella = document.getElementById('modalCierreEstrella');
 let flex5 = document.getElementById('flex5');
 let abrir5 = document.getElementById('abrir5');
 let cerrar5 = document.getElementById('close5');
+var animacionCierreDeEstrella = new Animador(30, 'img/animation/cierreDeEstrella/', "cierreDeEstrella",'close5');
 
 abrir5.addEventListener('click', function(){
     modalCierreEstrella.style.display = 'block';
+    animacionCierreDeEstrella.animate();
+
 });
 
 cerrar5.addEventListener('click', function(){
@@ -112,9 +127,11 @@ let modalPrecedencia = document.getElementById('modalPrecedencia');
 let flex6 = document.getElementById('flex6');
 let abrir6 = document.getElementById('abrir6');
 let cerrar6 = document.getElementById('close6');
+var animacionPrecedenciaDeOperadores = new Animador(40, 'img/animation/precedenciaDeOperadores/', "precedenciaDeOperadores",'close6');
 
 abrir6.addEventListener('click', function(){
     modalPrecedencia.style.display = 'block';
+    animacionPrecedenciaDeOperadores.animate();
 });
 
 cerrar6.addEventListener('click', function(){
@@ -134,9 +151,12 @@ let modalCompletez1 = document.getElementById('modalCompletez1');
 let flex7 = document.getElementById('flex7');
 let abrir7 = document.getElementById('abrir7');
 let cerrar7 = document.getElementById('close7');
+var animacionCompletez1 = new Animador(56, 'img/animation/condiciones/completez1/', "completez1",'close7');
 
 abrir7.addEventListener('click', function(){
     modalCompletez1.style.display = 'block';
+    animacionCompletez1.animate();
+
 });
 
 cerrar7.addEventListener('click', function(){
@@ -156,9 +176,11 @@ let modalCompletez2 = document.getElementById('modalCompletez2');
 let flex8 = document.getElementById('flex8');
 let abrir8 = document.getElementById('abrir8');
 let cerrar8 = document.getElementById('close8');
+var animacionCompletez2 = new Animador(56, 'img/animation/condiciones/completez2/', "completez2",'close8');
 
 abrir8.addEventListener('click', function(){
     modalCompletez2.style.display = 'block';
+    animacionCompletez2.animate();
 });
 
 cerrar8.addEventListener('click', function(){
@@ -173,14 +195,15 @@ window.addEventListener('click', function(e){
 });
 
 
-
 let modalCorreccion= document.getElementById('modalCorreccion');
 let flex9 = document.getElementById('flex9');
 let abrir9 = document.getElementById('abrir9');
 let cerrar9 = document.getElementById('close9');
+var animacionCorreccion = new Animador(29, 'img/animation/condiciones/correccion/', "correccion",'close9');
 
 abrir9.addEventListener('click', function(){
     modalCorreccion.style.display = 'block';
+    animacionCorreccion.animate();
 });
 
 cerrar9.addEventListener('click', function(){
@@ -200,9 +223,11 @@ let modalEjemplo1= document.getElementById('modalEjemplo1');
 let flex10 = document.getElementById('flex10');
 let abrir10 = document.getElementById('abrir10');
 let cerrar10 = document.getElementById('close10');
+var animacionEjemplo1 = new Animador(10, 'img/animation/ejemplo1/', "ejemplo1",'close10');
 
 abrir10.addEventListener('click', function(){
-    modalEjemplo1.style.display = 'block';
+        modalEjemplo1.style.display = 'block';
+        animacionEjemplo1.animate();
 });
 
 cerrar10.addEventListener('click', function(){
@@ -222,9 +247,11 @@ let modalEjemplo2= document.getElementById('modalEjemplo2');
 let flex11 = document.getElementById('flex11');
 let abrir11 = document.getElementById('abrir11');
 let cerrar11 = document.getElementById('close11');
+var animacionEjemplo2 = new Animador(10, 'img/animation/ejemplo2/', "ejemplo2",'close11');
 
 abrir11.addEventListener('click', function(){
     modalEjemplo2.style.display = 'block';
+    animacionEjemplo2.animate();
 });
 
 cerrar11.addEventListener('click', function(){
@@ -244,9 +271,11 @@ let modalEjemplo3= document.getElementById('modalEjemplo3');
 let flex12 = document.getElementById('flex12');
 let abrir12 = document.getElementById('abrir12');
 let cerrar12 = document.getElementById('close12');
+var animacionEjemplo3 = new Animador(13, 'img/animation/ejemplo3/', "ejemplo3",'close12');
 
 abrir12.addEventListener('click', function(){
     modalEjemplo3.style.display = 'block';
+    animacionEjemplo3.animate();
 });
 
 cerrar12.addEventListener('click', function(){
@@ -266,9 +295,11 @@ let modalEjemplo4= document.getElementById('modalEjemplo4');
 let flex13 = document.getElementById('flex13');
 let abrir13 = document.getElementById('abrir13');
 let cerrar13 = document.getElementById('close13');
+var animacionEjemplo4 = new Animador(13, 'img/animation/ejemplo4/', "ejemplo4",'close13');
 
 abrir13.addEventListener('click', function(){
     modalEjemplo4.style.display = 'block';
+    animacionEjemplo4.animate();
 });
 
 cerrar13.addEventListener('click', function(){
@@ -281,3 +312,7 @@ window.addEventListener('click', function(e){
         modalEjemplo4.style.display = 'none';
     }
 });
+
+
+////////////////////////////////////////////////////
+/* ESTA SECCIÓN ES PARA MODIFICAR MODALES PARA ANIMAR*/
